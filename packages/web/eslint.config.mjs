dedfbe878161d4acc859ea6623089f7d6d7ecf99
suzumiyaaoba <SuzumiyaAoba@gmail.com>
2025-12-09 20:54:40 +0900

    chore: add storybook setup

--- packages/web/eslint.config.mjs
+++ packages/web/eslint.config.mjs
@@ -1,28 +1,28 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import nextPlugin from '@next/eslint-plugin-next';
 import tsParser from '@typescript-eslint/parser';
 
-export default [
-  {
-    ignores: ['.next/', 'node_modules/', 'out/', 'public/'],
-  },
-  {
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
-        },
+export default [{
+  ignores: ['.next/', 'node_modules/', 'out/', 'public/'],
+}, {
+  files: ['**/*.{js,jsx,ts,tsx}'],
+  languageOptions: {
+    parser: tsParser,
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
       },
     },
-    plugins: {
-      '@next/next': nextPlugin,
-    },
-    rules: {
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs['core-web-vitals'].rules,
-    },
   },
-];
+  plugins: {
+    '@next/next': nextPlugin,
+  },
+  rules: {
+    ...nextPlugin.configs.recommended.rules,
+    ...nextPlugin.configs['core-web-vitals'].rules,
+  },
+}, ...storybook.configs["flat/recommended"]];
