17ef1ef41b05fac702f9e8b7568aeafc9fbc2a5f
suzumiyaaoba <SuzumiyaAoba@gmail.com>
2025-12-09 21:12:05 +0900

    chore: switch storybook builder to vite

--- packages/web/.storybook/main.ts
+++ packages/web/.storybook/main.ts
@@ -3,6 +3,11 @@ import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-a11y', '@storybook/addon-docs'],
-  framework: '@storybook/nextjs',
+  framework: {
+    name: '@storybook/nextjs',
+    options: {
+      builder: { name: '@storybook/builder-vite' },
+    },
+  },
 };
 export default config;
